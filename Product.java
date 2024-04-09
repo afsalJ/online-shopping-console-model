e5378214c9e7fc14f7e5412007891d50327becee
afsalj <mohamedafsal.jvf@gmail.com>
2024-04-09 09:45:36 +0530

    all files

---
+++ Product.java
@@ -0,0 +1,18 @@
+package onlineShopping;
+
+public class Product {
+	public String product_name;
+	public int price,quantity;
+	public String sellerName;
+	
+	public Product(String product_name,String seller,int price,int quantity) {
+		sellerName = seller;
+		this.product_name=product_name;
+		this.price=price;
+		this.quantity=quantity;
+	}
+	
+
+	}
+
+
